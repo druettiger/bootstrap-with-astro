d1cb8d9e53ee2b452e38db289f4c8ee1405afc17
Dominik Rüttiger <51407404+druettiger@users.noreply.github.com>
2024-12-17 22:08:52 +0100

    Silence SASS deprecation warnings
    - recommended by Bootstrap team
    - see https://github.com/twbs/bootstrap/issues/40962

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Silencing SASS deprecation warnings (recommended by Bootstrap team)
+  // see https://github.com/twbs/bootstrap/issues/40962
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import']
+        }
+      }
+    }
+  }
+});
