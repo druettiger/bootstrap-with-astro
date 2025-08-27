f8d4b453ecae23cede520de5e2630175ade55b1e
Dominik Rüttiger <51407404+druettiger@users.noreply.github.com>
2025-08-27 06:21:49 +0000

    Fix for port forwarding in VS Code Dev Containers
    - see https://www.drsys.de/dev-containers-with-astro-vite/

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  // Fix for port forwarding in VS Code Dev Containers
+  // see https://www.drsys.de/dev-containers-with-astro-vite/
+  server: {
+    host: '127.0.0.1'
+  },
   // Silencing SASS deprecation warnings (recommended by Bootstrap team)
   // see https://github.com/twbs/bootstrap/issues/40962
   vite: {
